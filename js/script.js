a7df8690f15a197fa821bebc5e8364ffa21f80cf
barrameda <hello@adambarrameda.com>
2023-08-11 14:40:38 -0400

    validating user input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedDisplay = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const input = document.querySelector("#letter");
 const scaffold = document.querySelector(".word-in-progress");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const setup = function (word) {
     const dots = [];
@@ -23,6 +24,33 @@ setup(word);
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     const guess = input.value;
-    console.log(guess);
+    // console.log(guess);
     input.value = "";
+    message.innerText = "";
+    const vald = inputVal(guess);
+    console.log(vald);
+    makeGuess(vald);
 });
+
+const inputVal = function (input) {
+    const acceptedLetter = /[a-zA-Z]/
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        message.innerText = "Just one letter, please.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter.";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You've already used this letter. Try again!"
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
